efe2b85bfdf01d932ffe6c99fade7db06390edc0
akila514 <akilajay514@gmail.com>
2023-10-14 18:14:56 +0530

    added

--- ProShop_FrontEnd/src/store/store.js
+++ ProShop_FrontEnd/src/store/store.js
@@ -5,6 +5,9 @@ const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
 export default store;
