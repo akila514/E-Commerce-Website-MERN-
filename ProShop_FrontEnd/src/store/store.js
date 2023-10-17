ca2d823efd621c0ffed161375935a9d293bc76cb
akila514 <akilajay514@gmail.com>
2023-10-18 02:13:50 +0530

    added userApiSlice

--- ProShop_FrontEnd/src/store/store.js
+++ ProShop_FrontEnd/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./apiSlice";
 import { cartReducer } from "./cartSlice";
+import { authReducer } from "./authSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartReducer,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
